f74b7161ab662557434d5589e74db1d565acf2f4
Siddhartha Singh <singh.si@husky.neu.edu>
2017-08-25 23:53:18 -0400

    final TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 /*
  * 
@@ -23,17 +24,15 @@ public int[] twoSum(int[] nums, int target) {
 		return empty;
 	}
 	
-	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-	for(int i=0;i< nums.length; i++) {
-		if(map.containsKey(target - nums[i])) {
-			return new int[] {target - nums[i], nums[i]};
-		}
-		else {
-			map.put(nums[i], i);
-		}
+	Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
     }
-	
-	return empty;
+    throw new IllegalArgumentException("No two sum solution");
 
 }
 
