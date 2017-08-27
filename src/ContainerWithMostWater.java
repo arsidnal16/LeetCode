783258f8e9b4191dcb8b6fb36b36857a5f504c5d
Siddhartha Singh <singh.si@husky.neu.edu>
2017-08-26 23:29:13 -0400

    3 sum

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -1,4 +1,18 @@
 
+/*
+ * 
+ * The intuition behind this approach is that the area formed 
+ * between the lines will always be limited by the height of the shorter line. 
+ * Further, the farther the lines, the more will be the area obtained.
+
+We take two pointers, one at the beginning and one at the end of the array constituting 
+the length of the lines. 
+Futher, we maintain a variable maxareamaxarea to store the maximum area obtained till now. 
+At every step, we find out the area formed between them, 
+update maxareamaxarea and move the pointer pointing to the shorter line 
+towards the other end by one step.
+ */
+
 public class ContainerWithMostWater {
 	
 	public int maxArea(int[] height) {
