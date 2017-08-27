2c477f33cafea9ab1c88de822bdd77de917b79e4
Siddhartha Singh <singh.si@husky.neu.edu>
2017-08-26 21:26:39 -0400

    Palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,29 @@
+
+public class Palindrome {
+	
+	public boolean isPalindrome(int x) {
+        //negative numbers are not palindrome
+		if (x < 0)
+			return false;
+
+		// initialize how many zeros
+		int div = 1;
+		while (x / div >= 10) {
+			div *= 10;
+		}
+
+		while (x != 0) {
+			int left = x / div;
+			int right = x % 10;
+
+			if (left != right)
+				return false;
+
+			x = (x % div) / 10;
+			div /= 100;
+		}
+
+		return true;
+   }
+
+}
