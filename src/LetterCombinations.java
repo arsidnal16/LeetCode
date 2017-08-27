242cd3fd1b9b6d37bb8d65da7248bdf2627f820d
Siddhartha Singh <singh.si@husky.neu.edu>
2017-08-27 00:13:46 -0400

    lettercombinations

---
+++ src/LetterCombinations.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LetterCombinations {
+	
+	public static List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        if (digits.length()==0){
+	        return ans;
+        }
+        
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        for(int i =0; i<digits.length();i++){
+            int x = Character.getNumericValue(digits.charAt(i));
+            while(ans.peek().length()==i){
+                String t = ans.remove();
+                for(char s : mapping[x].toCharArray())
+                  ans.add(t+s);
+            }
+        }
+    return ans;
+    }
+	
+	public static void main(String args[]) {
+		letterCombinations("2");
+	}
+
+}
