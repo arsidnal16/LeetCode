1a37684af4801d409bcf313497613093feb9d1db
Siddhartha Singh <singh.si@husky.neu.edu>
2017-10-03 21:49:08 -0400

    paranthesis

--- src/BalancedParanthesis.java
+++ src/BalancedParanthesis.java
@@ -2,30 +2,19 @@ import java.util.Stack;
 
 public class BalancedParanthesis {
 	
-	public static boolean isBalanced(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '[' || c == '(' || c == '{' ) {     
-                stack.push(c);
-            } else if(c == ']') {
-                if(stack.isEmpty() || stack.pop() != '[') {
-                    return false;
-                }
-            } else if(c == ')') {
-                if(stack.isEmpty() || stack.pop() != '(') {
-                    return false;
-                }           
-            } else if(c == '}') {
-                if(stack.isEmpty() || stack.pop() != '{') {
-                    return false;
-                }
-            }
-
-        }
-        return stack.isEmpty();
-    
-   
-    }
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '{')
+				stack.push('}');
+			else if (c == '[')
+				stack.push(']');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+		return stack.isEmpty();
+	}
 
 }
