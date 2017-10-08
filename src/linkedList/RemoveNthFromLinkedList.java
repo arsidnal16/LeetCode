b9068950c04dc825d2912fe4d593c6c3c9aada8f
Siddhartha Singh <singh.si@husky.neu.edu>
2017-10-08 12:49:56 -0400

    linkedlistvalidation

--- src/linkedList/RemoveNthFromLinkedList.java
+++ src/linkedList/RemoveNthFromLinkedList.java
@@ -1,30 +1,32 @@
 package linkedList;
+
 public class RemoveNthFromLinkedList {
-	
+
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        ListNode slow = head;
-        
-        while(n!=0){
-            fast = fast.next;
-            n--;
-        }
-        
-        if(fast == null){
-            head = head.next;
-            return head;
-        }
-        
-        
-        while(fast.next != null){
-            fast = fast.next;
-            slow = slow.next;
-        }
-        
-        slow.next = slow.next.next;
-        
-        return head;
-        
-    }
+		// If list does not exist or if there are no elements in the list,return NULL
+		if (head == null || n < 1) {
+			return null;
+		}
+
+		// make pointers p1 and p2 point to the start of the list.
+		ListNode p1 = head;
+		ListNode p2 = head;
+		for (int j = 0; j < n; ++j) { // make then n nodes apart.
+
+			// while moving p2 check if it becomes NULL, that is if it reaches the end
+			// of the list. That would mean the list has less than n nodes, so its not
+			// possible to find nth from last, so return NULL.
+			if (p2 == null) {
+				return null;
+			}
+			p2 = p2.next;
+		}
+		while (p2 != null) { // move till p2 goes past the end of the list.
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		return p1;
+
+	}
 
 }
