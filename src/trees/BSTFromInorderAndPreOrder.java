8da4159cf5d5f02cc72d8ec3686ccc3fc1b9cb03
Siddhartha Singh <singh.si@husky.neu.edu>
2017-10-13 13:52:13 -0400

    changes to previous code

--- src/trees/BSTFromInorderAndPreOrder.java
+++ src/trees/BSTFromInorderAndPreOrder.java
@@ -1,7 +1,5 @@
 package trees;
 
-
-
 /*
  * 
  * The basic idea is here:
@@ -15,32 +13,39 @@ Recursively doing this on subarrays, we can build a tree out of it :)
  */
 
 public class BSTFromInorderAndPreOrder {
-	
-	
+
 	public class TreeNode {
-		      int val;
-		     TreeNode left;
-		      TreeNode right;
-		      TreeNode(int x) { val = x; }
-		  }
-	
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
-	    if(preorder.length!=inorder.length) return null;
-	    return build(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
 	}
 
-	public TreeNode build(int [] preorder, int preLow, int preHigh, int[] inorder, int inLow, int inHigh){
-	    if(preLow>preHigh || inLow>inHigh) return null;
-	    TreeNode root = new TreeNode(preorder[preLow]);
-	   
-	    int inorderRoot = inLow;
-	    for(int i=inLow;i<=inHigh;i++){
-	        if(inorder[i]==root.val){ inorderRoot=i; break; }
-	    }
-	   
-	    int leftTreeLen = inorderRoot-inLow;
-	    root.left = build(preorder, preLow+1, preLow+leftTreeLen, inorder, inLow, inorderRoot-1);
-	    root.right = build(preorder, preLow+leftTreeLen+1, preHigh, inorder, inorderRoot+1, preHigh);       
-	    return root;        
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		if (preorder.length != inorder.length)
+			return null;
+		return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+	}
+
+	public TreeNode build(int[] preorder, int preLow, int preHigh, int[] inorder, int inLow, int inHigh) {
+		if (preLow > preHigh || inLow > inHigh)
+			return null;
+		TreeNode root = new TreeNode(preorder[preLow]);
+
+		int inorderRoot = inLow;
+		for (int i = inLow; i <= inHigh; i++) {
+			if (inorder[i] == root.val) {
+				inorderRoot = i;
+				break;
+			}
+		}
+
+		int leftTreeLen = inorderRoot - inLow;
+		root.left = build(preorder, preLow + 1, preLow + leftTreeLen, inorder, inLow, inorderRoot - 1);
+		root.right = build(preorder, preLow + leftTreeLen + 1, preHigh, inorder, inorderRoot + 1, preHigh);
+		return root;
 	}
 }
--- src/trees/InvertBinaryTree.java
+++ src/trees/InvertBinaryTree.java
@@ -20,26 +20,27 @@ to
  * 
  */
 
-
 public class InvertBinaryTree {
-	
-	public class TreeNode {
-		      int val;
-		      TreeNode left;
-		      TreeNode right;
-		      TreeNode(int x) { val = x; }
-		  }
-	
-	
-	public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
 
-         TreeNode left = root.left;
-          TreeNode right = root.right;
-        root.left = invertTree(right);
-        root.right = invertTree(left);
-        return root;
-    }
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+		root.left = invertTree(right);
+		root.right = invertTree(left);
+		return root;
+	}
 }
