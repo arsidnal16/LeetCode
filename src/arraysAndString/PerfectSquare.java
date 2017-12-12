0aeacdae16800c8f6ecc9f8f4dd598465c78d005
Siddhartha Singh <singh.si@husky.neu.edu>
2017-12-12 00:01:33 -0500

    perfect square

---
+++ src/arraysAndString/PerfectSquare.java
@@ -0,0 +1,20 @@
+package arraysAndString;
+
+public class PerfectSquare {
+	
+	public boolean isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+
+}
