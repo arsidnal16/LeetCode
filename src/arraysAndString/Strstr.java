efc0935519f9139673ee72d26b4245c451b6b219
Siddhartha Singh <singh.si@husky.neu.edu>
2018-01-17 22:54:21 -0500

    changes and adds

--- src/arraysAndString/Strstr.java
+++ src/arraysAndString/Strstr.java
@@ -11,6 +11,18 @@ public class Strstr {
 		    }
 		  }
 		}
+		
+		
+		Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
 	*/
 	
 	public int strStr(String haystack, String needle) {
--- src/matrix/SudokuSolver.java
+++ src/matrix/SudokuSolver.java
@@ -1,5 +1,7 @@
 package matrix;
 
+import java.util.HashSet;
+
 public class SudokuSolver {
 
 	public void solveSudoku(char[][] board) {
@@ -41,5 +43,26 @@ public class SudokuSolver {
 				return false; // check 3*3 block
 		}
 		return true;
+	}
+	
+	
+	
+	public boolean isValidSudoku(char[][] board) {
+	    for(int i = 0; i<9; i++){
+	        HashSet<Character> rows = new HashSet<Character>();
+	        HashSet<Character> columns = new HashSet<Character>();
+	        HashSet<Character> cube = new HashSet<Character>();
+	        for (int j = 0; j < 9;j++){
+	            if(board[i][j]!='.' && !rows.add(board[i][j]))
+	                return false;
+	            if(board[j][i]!='.' && !columns.add(board[j][i]))
+	                return false;
+	            int RowIndex = 3*(i/3);
+	            int ColIndex = 3*(i%3);
+	            if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+	                return false;
+	        }
+	    }
+	    return true;
 	}
 }
